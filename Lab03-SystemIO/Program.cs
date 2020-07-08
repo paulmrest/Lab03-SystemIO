0a5426b6bfdf13ca7e4d692b7f5f75156e2fc1f5
paulmrest <paulmrest@gmail.com>
2020-07-08 16:14:56 -0700

    Lab03 - Challenge 01

---
+++ Lab03-SystemIO/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab03_SystemIO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AskUserForThreeNumbers();
+        }
+
+        //Challenge 1
+        /// <summary>
+        /// Asks the user for three or more numbers, and returns their product.
+        /// </summary>
+        /// <remarks>
+        /// Uses ThreeNumbersProduct() to calculate the product.
+        /// </remarks>
+        static void AskUserForThreeNumbers()
+        {
+            Console.WriteLine("Fortune to you my good friend! We get to find out the proudct of three numbers in the most laborous modern way possible!\n");
+            Console.WriteLine("Please enter your three numbers separated by one or more spaces:");
+            string userInput = Console.ReadLine();
+            int product = ThreeNumbersProduct(userInput);
+            Console.WriteLine("Your product is: {0}", product);
+        }
+
+        /// <summary>
+        /// Takes a string of space-separated "words" and "multiples" the first three of
+        /// of those "words" together. If a given "word" can be parsed to a number value,
+        /// this is included in the final product. If a given "word" cannot be parsed
+        /// as a number, then we multiple the product by 1. IF the string contains less than
+        /// three "words", the function returns 0.
+        /// </summary>
+        /// <param name="input">A string consisting of one or more space-separated "words"</param>
+        /// <returns>The product of the parsed "words" from the input string.</returns>
+        public static int ThreeNumbersProduct(string input)
+        {
+            string[] spaceSep = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (spaceSep.Length < 3)
+            {
+                return 0;
+            }
+            int product = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                if (Int32.TryParse(spaceSep[i], out int result))
+                {
+                    product *= result;
+                }
+            }
+            return product;
+        }
+    }
+}
---
+++ Lab03-SystemIOTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit;
+using static Lab03_SystemIO.Program;
+
+namespace Lab03_SystemIOTest
+{
+    public class UnitTest1
+    {
+        //Challenge 1
+        [Fact]
+        public void ForStringOfLessThanThreeWordsReturnsZero()
+        {
+            //Arrange
+            string input = "heavy book";
+            //Act
+            int result = ThreeNumbersProduct(input);
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void ForStringOfThreeNonNumbersReturnsOne()
+        {
+            //Arrange
+            string input = "heavy book on the shelf";
+            //Act
+            int result = ThreeNumbersProduct(input);
+            //Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ForStringOfMoreThanThreeNumbersReturnsProductOfFirstThree()
+        {
+            //Arrange
+            string input = "4 3 6 7 5";
+            //Act
+            int result = ThreeNumbersProduct(input);
+            //Assert
+            Assert.Equal(72, result);
+        }
+
+        [Fact]
+        public void CanHandleMultipleSpacesBetweenNumbers()
+        {
+            //Arrange
+            string input = "4             3  6     7        5";
+            //Act
+            int result = ThreeNumbersProduct(input);
+            //Assert
+            Assert.Equal(72, result);
+        }
+
+        [Theory]
+        [InlineData("-6 -4 -3", -72)]
+        [InlineData("nope 4 maybe 5", 4)]
+        [InlineData("4 3 swish", 12)]
+        public void ThreeNumbersProductTest(string input, int expected)
+        {
+            int result = ThreeNumbersProduct(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
